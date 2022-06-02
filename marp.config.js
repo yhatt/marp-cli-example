6feab154df0afa230b9dc3f315f40c074dcb5680
Yuki Hattori <yukihattori1116@gmail.com>
2022-06-03 03:16:16 +0900

    Fix invalid reference

--- marp.config.js
+++ marp.config.js
@@ -2,7 +2,7 @@ const url = process.env.URL || process.env.VERCEL_URL || undefined
 
 module.exports = {
   allowLocalFiles: true,
-  ogImage: url && `${process.env.URL}/og-image.jpg`,
+  ogImage: url && `${url}/og-image.jpg`,
   themeSet: 'themes',
   url,
 }
